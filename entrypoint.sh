eb6b5ea57a26486b6f405f945d9285cc06286ec3
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-09-26 13:54:23 +0900

    Setup all

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,10 @@ fi
 $(npm bin)/textlint --version
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
-  # Use jq and github-pr-review reporter to format result to include link to rule page.
-  $(npm bin)/textlint -f compact \
+  $(npm bin)/textlint -f compact "${INPUT_TEXTLINT_FLAGS:-'.'}" \
     | reviewdog -efm="%f:%l:%c: %m" -name="textlint" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check (GitHub Check API) doesn't support markdown annotation.
-  $(npm bin)/textlint -f compact \
+  $(npm bin)/textlint -f compact "${INPUT_TEXTLINT_FLAGS:-'.'}" \
     | reviewdog -efm="%f:%l:%c: %m" -name="textlint" -reporter=github-pr-check -level="${INPUT_LEVEL}"
 fi
