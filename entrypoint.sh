d125c1925cbc610165e4d97834b6333373032e87
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-09-27 12:59:06 +0900

    fix reviewdog format

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,10 @@ fi
 $(npm bin)/textlint --version
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
-  $(npm bin)/textlint -f compact "${INPUT_TEXTLINT_FLAGS:-'.'}" \
-    | reviewdog -efm="%f:%l:%c: %m" -name="textlint" -reporter=github-pr-review -level="${INPUT_LEVEL}"
+  $(npm bin)/textlint -f checkstyle "${INPUT_TEXTLINT_FLAGS:-'.'}" \
+    | reviewdog -f=checkstyle -name="textlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check (GitHub Check API) doesn't support markdown annotation.
-  $(npm bin)/textlint -f compact "${INPUT_TEXTLINT_FLAGS:-'.'}" \
-    | reviewdog -efm="%f:%l:%c: %m" -name="textlint" -reporter=github-pr-check -level="${INPUT_LEVEL}"
+  $(npm bin)/textlint -f checkstyle "${INPUT_TEXTLINT_FLAGS:-'.'}" \
+    | reviewdog -f=checkstyle -name="textlint" -diff="git diff HEAD^" -reporter=github-pr-check  -level="${INPUT_LEVEL}"
 fi
