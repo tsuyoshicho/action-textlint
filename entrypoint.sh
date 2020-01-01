7cd40b2dde28b8a6dba5d1a376178ad3a93e6846
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-01-01 10:47:29 +0900

    work with new reporter

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,7 @@ fi
 
 $(npm bin)/textlint --version
 
-if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
-  $(npm bin)/textlint -f checkstyle "${INPUT_TEXTLINT_FLAGS:-'.'}" \
-    | reviewdog -f=checkstyle -name="textlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}"
-else
-  # github-pr-check (GitHub Check API) doesn't support markdown annotation.
-  $(npm bin)/textlint -f checkstyle "${INPUT_TEXTLINT_FLAGS:-'.'}" \
-    | reviewdog -f=checkstyle -name="textlint" -diff="git diff HEAD^" -reporter=github-pr-check  -level="${INPUT_LEVEL}"
-fi
+$(npm bin)/textlint -f checkstyle "${INPUT_TEXTLINT_FLAGS:-'.'}"    \
+  | reviewdog -f=checkstyle -name="textlint" -diff="git diff HEAD^" \
+              -reporter="${INPUT_REPORTER:-'github-pr-check'}"      \
+              -level="${INPUT_LEVEL:-'error'}"
