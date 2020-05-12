95bb906cc99080d076eec9e785ad7f80639a0d7c
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-05-12 22:07:35 +0900

    use npm ls

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,19 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # nothing command, try project package install
-if [ ! -f "$(npm bin)/textlint" ]; then
+if npm ls textlint &> /dev/null; then
+  # pass
+  :
+else
   npm install
 fi
 # nothing command in after install, use command direct install
 PACKAGE=""
-if [ ! -f "$(npm bin)/textlint" ]; then
+if npm ls textlint &> /dev/null; then
+  npx textlint --version
+else
   PACKAGE="-p textlint@${INPUT_TEXTLINT_VERSION:-11.6.3}"
   echo textlint direct install version: ${INPUT_TEXTLINT_VERSION:-11.6.3}
-else
-  npx textlint --version
 fi
 
 npx ${PACKAGE} textlint -f checkstyle "${INPUT_TEXTLINT_FLAGS:-.}" \
