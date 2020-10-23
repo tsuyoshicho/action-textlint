f2815aa5b35ca9432ff9657a5d8cca2759dea819
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-10-24 00:30:37 +0900

    fix for install

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,15 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # setup and check.
-npm ci
 if npm ls textlint &> /dev/null; then
-  :
   # pass
+  :
+else
+  npm ci
+fi
+
+if npm ls textlint &> /dev/null; then
+  npx textlint --version
 else
   echo This repository was not configured for textlint, process done.
   exit 1
